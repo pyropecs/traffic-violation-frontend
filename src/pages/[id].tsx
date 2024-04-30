fa6fe69a9c9b6c3c97596e433a1b86a6ea2b3b40
pyropecs <praveengopu0105@gmail.com>
2024-04-30 08:04:50 +0530

    final review

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,32 +1,36 @@
 import { useChallan } from "@/context";
 import {Button, Stack} from "@mantine/core"
-
+import "@/styles/challan.module.css"
 const Challan =()=>{
 const details = useChallan()
 const challan= details.challan;
-console.log(challan)
+
 return(
 <Stack justify="center" align="center" py="10px" px="20px" >
-	<div style={{width:"50%",border:"2px solid black",alignItems:"center",display:"flex",flexDirection:"column" }} className="column">
+	<div style={{width:"50%",border:"2px solid black",alignItems:"center",display:"flex",flexDirection:"column",padding:"10px" }} className="column">
 
         <div className="d-flex justify-content-between">
-            <strong>Invoice {challan.}</strong>
+            <strong>Invoice {challan.challan_id}</strong>
         </div>
         <div className="d-flex flex-col justify-content-center">
-            <h4 className="align-center title">Quran Education Online</h4>
+            <h4 className="align-center title">Goverment of country</h4>
         </div>
         <div className="d-flex justify-content-between">
-            <div className="d-flex flex-col">
-                <span>Due Date:</span>
-                <span>08/09/2022</span>
+            <div style={{display:"flex",gap:"20px",justifyContent:"space-between"}} >
+                <div>Due Date:</div>
+                <div>31/05/2023</div>
             </div>
-            <div className="d-flex flex-col">
-                <span>Account: 75-HBL</span>
-                <span>HMC Taxila</span>
+            <div style={{display:"flex",gap:"20px",justifyContent:"space-between"}}>
+                <span>Plate Number:</span>
+                <span>{challan.plate_number}</span>
             </div>
+            <div style={{display:"flex",gap:"20px",justifyContent:"space-between"}}>
+              <span>Owner Name: </span>
+            <span>{challan.owner_name}</span>
         </div>
-        <div  className="d-flex flex-col justify-content-center">
-            <img height="20vh" src="https://i.ibb.co/c8CQvBq/barcode.png" alt="bar code" className="bar-code"/>
+        </div>
+        <div style={{marginBlock:"10px"}}  className="d-flex flex-col justify-content-center">
+            <img height="20vh" src="https://i.ibb.co/c8CQvBq/barcode.png" alt="bar code" style={{width:"200px"}} />
         </div>
         <table>
             <thead>
@@ -37,20 +41,20 @@ return(
             </thead>
             <tbody>
                 <tr>
-                <td data-label="Account" colSpan={2}>Electricity-319315001<br/>Consumption</td>
-                <td data-label="Amount">500.0</td>
+                <td data-label="Account" colSpan={2}>{challan.violations[0]}</td>
+                <td data-label="Amount">{challan.fine_amount}</td>
                 </tr>
             </tbody>
         </table>
         <hr/>
         <div className="d-flex justify-content-between">
             <strong>Payable by due date</strong>
-            <span>Rs. 14,530</span>
+            <span> Rs. {challan.fine_amount}</span>
         </div>
         <hr/>
         <div className="d-flex justify-content-between">
             <strong>Payable after due date</strong>
-            <span>14,580.0</span>
+            <span> Rs.{Number(challan.fine_amount) + 500}</span>
         </div>
         <br/>
         <hr/>
